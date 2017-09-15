1cd352b28b76ef3cf33fc0540c86179761138632
gokturkenez <hi@gokturkenez.com.tr>
2017-09-15 11:10:49 +0300

    Hash issue fixed with Param sorting.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# PayU_TR_IRN_API_PythonSample
-PayU Turkey IRN API Python Sample Code
+# PayU Turkey IRN API Python 3.x Sample Code
+
+PayU Turkey IRN API Python 3 Sample Code v1.0
+    
+    * This is an **unofficial** sample code.
+    * This version is a release version, there is can be some fix needs, just let me know if you face 
+    with something wrong.
+    * This sample code developed on Python 3.4.2
+    * Below libraries used when developing
+        datetime > datetime
+        hmac
+        hashlib
+        urllib.parse > urlencode
+        urllib.request > Request, urlopen
+    * We are not owner any used libraries, all rights reserved to the owners.
+    * Datetime can be changed due to your system.
+    
+PayU Turkey IRN API Python 3 Sample Code v1.1
+    
+    * Hash issue fixed with Param sorting.
+    * Below library added for sorting params to ordered
+        collections
+      
---
+++ irn.py
@@ -0,0 +1,48 @@
+'''
+Project: PayU Turkey IRN API Python3 Sample Code
+Author: Göktürk Enez
+'''
+# Importing required libraries.
+from datetime import datetime
+import hmac
+import hashlib
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+import collections
+#Endpoint
+url = 'https://secure.payu.com.tr/order/irn.php'
+
+#Secret Key
+secret = '9g5^4+54@T8=!6_g+a9+'
+
+array = collections.OrderedDict()
+array['MERCHANT'] = 'PYKDMNSZ'
+array['ORDER_REF'] = '39537992'
+array['ORDER_AMOUNT'] = '129.33'
+array['ORDER_CURRENCY'] = 'TRY'
+array['IRN_DATE'] = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S')
+array['AMOUNT'] = '10'
+
+# Initializing the hashstring @param
+hashstring = ''
+#Order Directory
+# Getting Array @params
+for k, v in array.items():
+# Adding the UTF-8 byte length of each field value at the beginning of field value
+    hashstring += str(len(v)) + str(v)
+print(hashstring)
+
+# Calculating signature
+signature = hmac.new(secret.encode('utf-8'), hashstring.encode('utf-8'), hashlib.md5).hexdigest()
+
+# Adding signature @param to request array
+array['ORDER_HASH'] = signature
+print(signature)
+
+# Sending Request to Endpoint
+request = Request(url, urlencode(array).encode())
+response = urlopen(request).read().decode()
+
+# Printing result/response
+print(response)
+
